1de5333379b89ed369235777d55c24750d176838
Auke Haver <33574388+AukeHaver@users.noreply.github.com>
2022-06-14 23:11:34 +0200

    changed outfile specification and added output matrices

--- R/packages.R
+++ R/packages.R
@@ -2,13 +2,13 @@
 ### Author: Auke Haver
 ### BDA GROUP SILS Amsterdam
 ### Project: Zero-inflated GLMM ASCA
-### Date 
 
 library("tidyverse") # General IO
 library("ggplot2")
 library("ggpubr") # Extension on ggplot2
 library("glmmTMB") # zero-inflated GLMM fitting
 library("parallel") # Multi-core processing
+library("writexl")
 
 # Virids color objects 
 dark_purple <- rgb(72/255,40/255,120/255)
